cb082f4cd6cc75baf21700777b9312914b1f863c
Leomelonseeds <leonardfang15@gmail.com>
2022-09-17 14:06:59 -0700

    First commit

---
+++ src/main/java/com/leomelonseeds/afr/AgentFrakcioRaktar.java
@@ -0,0 +1,14 @@
+package com.leomelonseeds.afr;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AgentFrakcioRaktar extends JavaPlugin {
+    
+    public void onEnable() {
+        
+    }
+    
+    public void onDisable() {
+        
+    }
+}
